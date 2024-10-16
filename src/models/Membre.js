401bfcfeb2a90515f0dfd857f58fd1e3515added
Rekuiem84 <morinijoshua@gmail.com>
2024-10-16 18:23:12 +0200

    ajout d'un id à chaque membre

--- src/models/Membre.js
+++ src/models/Membre.js
@@ -1,5 +1,8 @@
+let id = 0;
+
 export default class Membre {
 	constructor(nom, nePayePas = [], payeSeul = [], partage = []) {
+		this.id = id++;
 		this.nom = nom;
 		this.nePayePas = nePayePas;
 		this.payeSeul = payeSeul;
